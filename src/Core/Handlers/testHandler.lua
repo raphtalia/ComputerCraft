ffce07a261b31a35a9d5e7322d0166a92f36e4db
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-06-18 19:30:15 -0500

    Update testHandler.lua

--- src/Core/Handlers/testHandler.lua
+++ src/Core/Handlers/testHandler.lua
@@ -1,1 +1,20 @@
 print("im a test handler")
+
+local Enum = require("Enum")
+
+local enum = Enum.new(
+    "testEnum",
+    {
+        "option1",
+        "option2",
+        "option3",
+    }
+)
+
+print(Enum)
+print(Enum.testEnum)
+print(Enum.testEnum.option1)
+print(enum)
+print(enum.option1)
+print(enum.option1 == enum.option1)
+print(enum.option1 ~= enum.option1)
